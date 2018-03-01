333d92caff9c36d113c66674e9f01ae52cbd8556
水野谷剛 <mizunoya0227@gmail.com>
2018-03-01 23:03:12 +0900

    ドアに近づくとアイコンが出るようにしました

---
+++ Assets/doorImageScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorImageScript : MonoBehaviour {
+    public bool inflag = false;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") inflag = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player") inflag = false;
+    }
+}
---
+++ Assets/inoutScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class inoutScript : MonoBehaviour {
+
+	float alfa;    //A値を操作するための変数
+    float red, green, blue;    //RGBを操作するための変数
+    public GameObject target;
+    doorImageScript DoorImage_;
+    // Use this for initialization
+    void Start()
+    {
+        //Panelの色を取得
+        alfa = 1;
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+        DoorImage_ = target.GetComponent<doorImageScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        print(alfa);
+        if (DoorImage_.inflag == true)
+        {
+
+            GetComponent<Image>().color = new Color(red, green, blue, alfa);
+            alfa = 1;
+           
+        }
+        else if (DoorImage_.inflag == false)
+        {
+            GetComponent<Image>().color = new Color(red, green, blue, alfa);
+            alfa = 0;
+          
+        }
+    }
+}
